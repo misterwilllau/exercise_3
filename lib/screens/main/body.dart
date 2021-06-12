c482478fa3cab19991f091c0d006969581123ace
WILLIAM LAU A18CS3039 <lau.william@graduate.utm.my>
2021-06-12 11:11:17 +0800

    Screens class

--- lib/screens/main/body.dart
+++ lib/screens/main/body.dart
@@ -23,18 +23,50 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<List<Todo>>(
+        future: _state.todoListFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            _state.todoList = snapshot.data;
+            return _buildListView();
+          }
+          return Center(
+            child: Text('Please Login'),
+          );
+        });
+  }
+
+  ListView _buildListView() {
     return ListView.separated(
-      itemCount: 5,
+      itemCount: _state.todoList.length,
       separatorBuilder: (context, index) => Divider(
         color: Colors.blueGrey,
       ),
       itemBuilder: (context, index) => ListTile(
-        title: Text('Todo title',
-            style: TextStyle(decoration: TextDecoration.lineThrough)),
-        subtitle: Text('Todo description'),
-        onTap: () {},
-        onLongPress: () {},
+        title: Text('${_state.todoList[index].title}',
+            style: TextStyle(
+                decoration: _state.todoList[index].done == true
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none)),
+        subtitle: Text('${_state.todoList[index].description}'),
+        onTap: () => _todoTap(context, index),
+        onLongPress: () => _todoLongPressed(index),
       ),
     );
+  }
+
+  void _todoTap(BuildContext context, int _index) async {
+    //This _todo only contain the updated attribute. (Desc, Title & Done)
+    final _todo = await Navigator.pushNamed(context, '/edit',
+        arguments: _state.todoList[_index]);
+    if (_todo != null) {
+      _state.updateTodo(index: _index, todo: _todo);
+    }
+  }
+
+  void _todoLongPressed(int index) {
+    if (_state.todoList[index].done == true) {
+      _state.removeTodo(index);
+    }
   }
 }
--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -52,9 +52,48 @@ class MainScreen extends StatefulWidget {
 }
 
 class MainScreenState extends State<MainScreen> {
-  void addTodo(Todo todo) async {}
-  void updateTodo({int index, Todo todo}) async {}
-  void removeTodo(int index) async {}
+  User _user;
+  List<Todo> _todoList;
+  Future<List<Todo>> _todoListFuture;
+
+  get user => _user;
+  set user(User value) {
+    _user = value;
+    refreshTodoListFuture();
+  }
+
+  get todoList => _todoList;
+  set todoList(value) => _todoList = value;
+
+  get todoListFuture => _todoListFuture;
+  set todoListFuture(value) => _todoListFuture = value;
+
+  void refreshTodoListFuture() {
+    if (_user != null) {
+      _todoListFuture = TodoService.getTodoListByUser(_user.id);
+      setState(() {});
+    }
+  }
+
+  void addTodo(Todo todo) async {
+    if (_user != null) {
+      todo.user = _user.id;
+      final Todo _todo = await TodoService.addTodo(todo);
+      setState(() => _todoList.add(_todo));
+    }
+  }
+
+  void updateTodo({int index, Todo todo}) async {
+    todo.id = _todoList[index].id;
+    todo.user = _todoList[index].user;
+    _todoList[index] = await TodoService.updateTodo(todo);
+    refreshTodoListFuture();
+  }
+
+  void removeTodo(int index) async {
+    await TodoService.removeTodo(_todoList[index]);
+    refreshTodoListFuture();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,9 +101,23 @@ class MainScreenState extends State<MainScreen> {
       onWillPop: () => Future.value(false),
       child: SafeArea(
         child: Scaffold(
-          appBar: Bar(),
-          body: Body(),
-          floatingActionButton: Float(),
+          appBar: Bar(
+            state: this,
+          ),
+          body: _user != null
+              ? Body(
+                  state: this,
+                )
+              : Container(
+                  child: Center(
+                    child: Text('Please Login First'),
+                  ),
+                ),
+          floatingActionButton: _user != null
+              ? Float(
+                  state: this,
+                )
+              : null,
         ),
       ),
     );
