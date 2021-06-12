bdb89acf4cf9adaf7f9a5b0dc3df5a82e37a2555
WILLIAM LAU A18CS3039 <lau.william@graduate.utm.my>
2021-06-12 11:01:35 +0800

    Screen class edit

--- lib/screens/edit/bar.dart
+++ lib/screens/edit/bar.dart
@@ -19,7 +19,10 @@ class Bar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text('The title goes here'),
+      // a. The title bar will show accordingly: 'Edit a todo' or 'Add a new todo'
+      title: _state.isEditing == true
+          ? Text('Edit a todo')
+          : Text('Add a new todo'),
       leading: Container(),
       centerTitle: true,
     );
