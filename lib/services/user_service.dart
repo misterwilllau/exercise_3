4159c8e7910f70f6fef08458e159adcbd58846bb
WILLIAM LAU A18CS3039 <lau.william@graduate.utm.my>
2021-06-12 11:25:24 +0800

    Services class todo_service.dart and user_service.dart

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -13,5 +13,11 @@ import '../models/user.dart';
 
 class UserService {
   static Future<User> getUserByLoginAndPassword(
-      {String login, String password}) async {}
+      {String login, String password}) async {
+    final user = await Rest.get('users?login=$login&password=$password');
+    if (user.length != 0) {
+      return User.fromJson(user[0]);
+    }
+    return null;
+  }
 }
